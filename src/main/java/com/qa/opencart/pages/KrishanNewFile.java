316e970e26a9c4450cf9eb5de34eaeac26b11d12
krishanmishra <kkmjssate@gmail.com>
2022-07-19 16:28:07 +0530

    Update KrishanNewFile.java

--- src/main/java/com/qa/opencart/pages/KrishanNewFile.java
+++ src/main/java/com/qa/opencart/pages/KrishanNewFile.java
@@ -1,6 +1,13 @@
-public static void main(String[] args) {
+package com.qa.opencart.pages;
+
+public class KrishanNewFile {
+
+	public static void main(String[] args) {
 
       int i= 10;
       System.out.println("print "+i);
 
 	}
+
+}
+
